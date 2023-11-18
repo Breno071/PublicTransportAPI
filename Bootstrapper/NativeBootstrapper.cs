7e95be049700fe822aa96ddbea11655de11d0314
Breno Narde Silva <brenonarde@gmail.com>
2023-11-18 13:35:46 -0300

    feat: add bootstrapper para injeção de dependencia

---
+++ Bootstrapper/NativeBootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Data;
+using System.Text.Json.Serialization;
+
+namespace PublicTransportAPI.Bootstrapper
+{
+    public static class NativeBootstrapper
+    { 
+        internal static void ConfigureService(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<PublicTransportAPIContext>(options =>
+            //options.UseInMemoryDatabase("db"));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("PublicTransportAPIContext") ?? throw new InvalidOperationException("Connection string 'PublicTransportAPIContext' not found.")));
+
+
+            // Add builder.Services to the container.
+            builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+            builder.Services.AddControllers()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Repository.Data;
-using System.Text.Json.Serialization;
+﻿using PublicTransportAPI.Bootstrapper;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<PublicTransportAPIContext>(options =>
-    //options.UseInMemoryDatabase("db"));
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PublicTransportAPIContext") ?? throw new InvalidOperationException("Connection string 'PublicTransportAPIContext' not found.")));
 
-
-
-// Add services to the container.
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-
-builder.Services.AddControllers()
-    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+NativeBootstrapper.ConfigureService(builder);
 
 var app = builder.Build();
 
